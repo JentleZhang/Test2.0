96663d52500301e51f83e80ecd40018021bf1b29
Bronson Zhang <25495422@qq.com>
2020-01-03 15:37:40 +0800

    test for using git by idea third

---
+++ src/bean/Customer.java
@@ -0,0 +1,31 @@
+package bean;
+
+public class Customer {
+    private Integer id;
+    private String agender;
+    private String name;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getAgender() {
+        return agender;
+    }
+
+    public void setAgender(String agender) {
+        this.agender = agender;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
